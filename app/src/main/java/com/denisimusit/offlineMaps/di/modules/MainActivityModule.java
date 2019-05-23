8e6b81f538ad2e20f6a72a39b9fa52323b8399b5
develop.qt@gmail.com <LenovoA706>
2019-05-23 19:22:43 +0300

    add     RegionsFragment regionsFragment();

--- app/src/main/java/com/denisimusit/offlineMaps/di/modules/MainActivityModule.java
+++ app/src/main/java/com/denisimusit/offlineMaps/di/modules/MainActivityModule.java
@@ -2,6 +2,7 @@ package com.denisimusit.offlineMaps.di.modules;
 
 import com.denisimusit.offlineMaps.di.scopes.FragmentScope;
 import com.denisimusit.offlineMaps.ui.fragments.home.view.HomeFragment;
+import com.denisimusit.offlineMaps.ui.fragments.regions.view.RegionsFragment;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
@@ -13,6 +14,10 @@ public interface MainActivityModule {
     @FragmentScope
     @ContributesAndroidInjector
     HomeFragment homeFragment();
+
+    @FragmentScope
+    @ContributesAndroidInjector
+    RegionsFragment regionsFragment();
 
 
 
